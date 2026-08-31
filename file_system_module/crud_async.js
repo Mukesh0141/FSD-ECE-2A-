43d0f06998f4b14a012f7fbf6d439f63a71ddda9
Mukesh Gupta <mukesh8287579373@gmail.com>
2026-08-31 09:30:34 +0530

    Fix error handling in file write and read operations

--- file_system_module/crud_async.js
+++ file_system_module/crud_async.js
@@ -1,5 +1,17 @@
 const fs = require("fs");
 
 fs.writeFile("notes.txt", "ECE A", (err) => {
+    if(err){
+        console.log(err);
+        return;
+    }
     console.log("Data Successfully Written in the file notes.txt")
+})
+
+fs.readFile("notes.txt", (err, data) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(`read data : ${data}`)
 })
