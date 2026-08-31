7187deeaa67f6802878a858ccbdd7d1245c88dc8
Mukesh Gupta <mukesh8287579373@gmail.com>
2026-08-31 09:55:31 +0530

    Add initial implementation for reading file content

---
+++ file_system_module/crud_promises.js
@@ -0,0 +1,8 @@
+import fs from 'fs/promises'
+
+async function readFileContent(filename){
+    const data = await fs.readFile(filename, "utf8")
+    console.log(data);
+}
+
+readFileContent("notes.txt");
